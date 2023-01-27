1ef35a2e07dd29fc535a38762ebf4d42c575ab08
KarthikaVC <karthikav.rmkcse@gmail.com>
2023-01-28 07:10:33 +0800

    Adding pledge create in url.py

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('pledges/<int:pk>/', views.PledgeDetail.as_view(),name='pledge-detail'),
     path('pledges/<int:pk>/editPledge',views.UpdatePledge.as_view(),name='pledge-update'),
     path('pledges/<int:pk>/deletePledge',views.DeletePledge.as_view(),name='pledge-delete'),
-    path('projects/<int:pk>/deleteProject', views.DeleteProject.as_view(), name='project-delete')
+    path('projects/<int:pk>/deleteProject', views.DeleteProject.as_view(), name='project-delete'),
+    path('pledges/create',views.PledgeList.as_view(), name='pledge-create'),
     ]
 urlpatterns = format_suffix_patterns(urlpatterns)
